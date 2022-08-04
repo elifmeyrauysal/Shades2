e844a58d7a5e32ca9aa624e18004b26fa8433900
elifmeyrauysal <elifmeyrauysal@gmail.com>
2022-08-04 21:43:52 +0300

    Initial Commit

---
+++ Shades2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shades2
+//
+//  Created by Uysal on 4.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
